325c229aab295e3a8c567ae07534df9d77158ff3
Czarna Owca <szydlowska_klaudia@protonmail.com>
2021-12-28 13:30:37 +0100

    refaktor test todoapp i 3items

--- cypress/integration/todoapp/3items.js
+++ cypress/integration/todoapp/3items.js
@@ -1,40 +1,35 @@
 describe("when add 3 items and tick checkbox -one of them", () => {
-    beforeEach(() => {
-        cy.visit("https://todomvc.com/examples/vanillajs/")
-    })
-    const addNewItem = (itemName) => {
-        cy.get("input.new-todo").type(itemName).type("{enter}");
-    }
     const firstItem = "aaa";
     const secondItem = "bbb";
     const thirdItem = "ccc";
+    const addNewItem = (itemName) => { cy.get("input.new-todo").type(itemName).type("{enter}")}
 
     beforeEach(() => {
-        
+        cy.visit("https://todomvc.com/examples/vanillajs/")
 
         addNewItem(firstItem);
         cy.get("input[class='toggle']").check()
         addNewItem(secondItem);
         addNewItem(thirdItem);
-
     })
 
-
-        
     context("when click on button-Active", () => {
         it("should - be seen 2 items: second item bbb and third item ccc", () => {
-    
-         cy.get('a').contains('Active').click()
-         cy.get(".todo-list li").eq(0).should("contain.text", secondItem)
-         cy.get(".todo-list li").eq(1).should("contain.text", thirdItem)
-         cy.get("span[class='todo-count']").find("strong").should("contain.text", 2)
-        
-         cy.get('firstItem').should('not.exist');
-        
-        })
            
+            cy.get('a').contains('Active').click()
+            cy.get(".todo-list li").eq(0).should("contain.text", secondItem)
+            cy.get(".todo-list li").eq(1).should("contain.text", thirdItem)
+            cy.get("span[class='todo-count']").find("strong").should("contain.text", 2)
+            cy.get('firstItem').should('not.exist');
+        })
+
     })
 
-    
+    context("when click on button - Completed ", () => {
+        it("should be marked first item-aaa ", () => {
+
+            cy.get('a').contains('Completed').click()
+        })
+    })
 
 })
--- cypress/integration/todoapp/todoapp_spec.js
+++ cypress/integration/todoapp/todoapp_spec.js
@@ -1,38 +1,38 @@
 describe("ToDo app", () => {
+
+    const addNewItem = (itemName) => {
+        cy.get("input.new-todo").type(itemName).type("{enter}");
+    }
+
     beforeEach(() => {
         cy.visit("https://todomvc.com/examples/vanillajs/");
 
-        context("when page initially loaded", () => {
-            //beforeEach(() => {
-            //   cy.visit("https://todomvc.com/examples/vanillajs/");
-            // cy.pause();
-            // cy.screenshot();
-        })
+    })
+
+
+    context("when page initially loaded", () => {
+
         it("heder of the page should be visible and have proper name", () => {
             cy.get(" .header h1").should("be.visible").and("contain.text", "todos")
         })
 
         it("input should have proper placeholder", function () {
-            //cy.get('input').invoke('attr', 'placeholder').should('contain', 'What needs to be done?')
+
             cy.get('input').should("have.attr", "placeholder", "What needs to be done?")
 
         })
+
     })
-    const addNewItem = (itemName) => {
-        cy.get("input.new-todo").type(itemName).type("{enter}");
-    }
+
+
     context("when added new item", () => {
-        //beforeEach(() => {
-        //  cy.visit("https://todomvc.com/examples/vanillajs/");
-        //  })
+
         it("should be added only one item", () => {
             cy.get("input.new-todo").type("Hello!").type("{enter}")
             cy.get(".todo-list li").should("have.length", 1)
         })
 
         it("should add 3 items with proper order", () => {
-
-
             const firstItem = "first Item";
             const secondItem = "second Item";
             const thirdItem = "third Item";
@@ -40,8 +40,6 @@ describe("ToDo app", () => {
             addNewItem(firstItem);
             addNewItem(secondItem);
             addNewItem(thirdItem);
-
-
 
             cy.get(".todo-list li").eq(0).should("contain.text", firstItem);
             cy.get(".todo-list li").eq(1).should("contain.text", secondItem);
@@ -58,27 +56,28 @@ describe("ToDo app", () => {
             })
         })
     })
+
     context("when click checkboxes near added item", () => {
-        
-            it("should ticked  checkbox and change number of Left items to 0", () => {
-                
-                addNewItem("first")
 
-                cy.get("input[class='toggle']").check()
-                cy.get("span[class='todo-count']").find("strong").should("contain.text", 0)
-            })
-            
-            it("should add tekst Clear completed", () => {
-                
-                addNewItem("first")
-                               
-                cy.get("input[class='toggle']").check()
-                cy.get("button[class='clear-completed']").should('have.attr', 'style','display: block;')
-                cy.contains('Clear completed').should('be.visible')
-                
-            })
+        it("should ticked  checkbox and change number of Left items to 0", () => {
 
-    context("when add 3 items and tick checkbox -one of them", () => {
+            addNewItem("first")
+
+            cy.get("input[class='toggle']").check()
+            cy.get("span[class='todo-count']").find("strong").should("contain.text", 0)
+        })
+
+        it("should add tekst Clear completed", () => {
+
+            addNewItem("first")
+
+            cy.get("input[class='toggle']").check()
+            cy.get("button[class='clear-completed']").should('have.attr', 'style', 'display: block;')
+            cy.contains('Clear completed').should('be.visible')
+
+        })
+
+        context("when add 3 items and tick checkbox -one of them", () => {
 
             it("should number of left items should be only 2", () => {
 
@@ -88,12 +87,10 @@ describe("ToDo app", () => {
                 addNewItem("ccc")
                 cy.get("span[class='todo-count']").find("strong").should("contain.text", 2)
 
-                
+
             })
 
-
-    })
-
+        })
 
     })
 })
