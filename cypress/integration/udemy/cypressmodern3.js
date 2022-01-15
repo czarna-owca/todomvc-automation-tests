86f0ad176b722aa67bb8c51f601efc3531f6db2a
czarna-owca <szydlowska_klaudia@protonmail.com>
2022-01-15 18:25:27 +0100

    cypressmodern3 - checkboxes

---
+++ cypress/integration/udemy/cypressmodern3.js
@@ -0,0 +1,11 @@
+
+describe(" Third Test Suite", () => {
+    it("third case", () => {
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+    })
+
+})
