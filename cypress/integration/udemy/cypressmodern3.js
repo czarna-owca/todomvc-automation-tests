e4f7898487bfd5cf806380639401f27843a6b5ed
czarna-owca <szydlowska_klaudia@protonmail.com>
2022-01-15 18:31:23 +0100

    cypressmodern3 - select

--- cypress/integration/udemy/cypressmodern3.js
+++ cypress/integration/udemy/cypressmodern3.js
@@ -6,6 +6,10 @@ describe(" Third Test Suite", () => {
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
         cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+
+
+        cy.get('select').select('option2').should('have.value', 'option2')
+
     })
 
 })
