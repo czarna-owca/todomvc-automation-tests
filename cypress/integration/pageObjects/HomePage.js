d16024fe92762aabd39caa1cc8a500ebeeae2b52
Czarna Owca <szydlowska_klaudia@protonmail.com>
2022-01-26 14:28:45 +0100

    cypressmodern7 - productpage.js

--- cypress/integration/pageObjects/HomePage.js
+++ cypress/integration/pageObjects/HomePage.js
@@ -20,6 +20,12 @@ getEntrepreneaur()
 {
     return cy.get('#inlineRadio3')
 }
+
+getShopTab()
+{
+    return cy.get(':nth-child(2) > .nav-link')
+}
+
 }
 
 export default HomePage;
--- cypress/integration/udemy/cypressmodern7.js
+++ cypress/integration/udemy/cypressmodern7.js
@@ -1,5 +1,6 @@
 
 import HomePage from '../pageObjects/HomePage'
+import ProductPage from '../pageObjects/ProductPage'
 describe("7 Test Suite", () => {
     before(function () {
         cy.fixture('example').then(function (data) {
@@ -10,21 +11,25 @@ describe("7 Test Suite", () => {
 
     it('7 case', function () {
         const homePage=new HomePage()
+        const productPage=new ProductPage()
 
         cy.visit("https://rahulshettyacademy.com/angularpractice/")
 
 
-        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
-        cy.get('select').select(this.data.gender)
-        cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
-        cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2')
-        cy.get('#inlineRadio3').should('be.disabled')
+        homePage.getEditBox().type(this.data.name)
+        homePage.getGender().select(this.data.gender)
+        homePage.getTwoWayDataBinding().should('have.value', this.data.name)
+        homePage.getEditBox().should('have.attr', 'minlength', '2')
+        homePage.getEntrepreneaur().should('be.disabled')
 
-        cy.get(':nth-child(2) > .nav-link').click()
+        homePage.getShopTab().click()
 
         this.data.productName.forEach(function (element) {
             cy.selectProduct(element)
         })
+       
+        productPage.checkOutButton().click()
+       // cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').click()
 
     })
 })
