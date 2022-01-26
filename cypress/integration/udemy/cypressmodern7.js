e095aac9cd7a4b5880bb0366be3be0bdf939f505
Czarna Owca <szydlowska_klaudia@protonmail.com>
2022-01-26 11:33:45 +0100

    cypressmodern7 - command

--- cypress/integration/udemy/cypressmodern7.js
+++ cypress/integration/udemy/cypressmodern7.js
@@ -1,20 +1,25 @@
-describe(" 7 Test Suite", () => {
+describe("7 Test Suite", () => {
     before(function () {
         cy.fixture('example').then(function(data)
          {
-            this.data=data
+    this.data=data
         })
 
     })
     
-    it("7 case", () => {
+    it('7 case', function () {
 
         cy.visit("https://rahulshettyacademy.com/angularpractice/")
-        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)  //("biok") //(this.data.name)
-        cy.get('select').select("Female")
-        //(this.data.gender)
-        //("Female") //(
+        
+    cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+    cy.get('select').select(this.data.gender)
+    cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name) 
+    cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength','2')
+    cy.get('#inlineRadio3').should('be.disabled')
+    
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.selectProduct('BlackBerry')
 
-
+    
     })
 })
