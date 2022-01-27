1d1af635259b21979e6454a96e16a76a820989a5
Czarna Owca <szydlowska_klaudia@protonmail.com>
2022-01-27 10:55:59 +0100

    cypressmodern7 - total amount

--- cypress/integration/udemy/cypressmodern7.js
+++ cypress/integration/udemy/cypressmodern7.js
@@ -34,13 +34,30 @@ describe("7 Test Suite", () => {
         })
 
         productPage.checkOutButton().click()
-        
+    
+        var sum=0
+
         cy.get('tr td:nth-child(4) strong').each(($el, index, $list) => {
+           
+            const amount= $el.text()
+            var res = amount.split(" ")
+            res = res[1].trim()
+            sum=Number(sum)+Number(res)
+                     
 
-            cy.log($el.text())
-
+        }).then(function(){
+        
+                cy.log(sum)
         })
 
+        cy.get('h3 strong').then(function(element)
+        {
+            const amount= element.text()
+            var res = amount.split(" ")
+           var total = res[1].trim()
+           
+           expect(Number(total)).to.equal(sum)
+        })
 
         cartPage.checkOutSuccess().click() //= cy.contains('Checkout').click()
 
@@ -50,12 +67,13 @@ describe("7 Test Suite", () => {
         cy.get('#checkbox2').click({ force: true })
         cy.get('input[type="submit"]').click()
         //cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :-).')
+
         cy.get('.alert').then(function (element) {
-            const actualText = element.text()
+            const actualText=element.text()
             expect(actualText.includes("Success")).to.be.true
         })
 
-        
+
     })
 
 
