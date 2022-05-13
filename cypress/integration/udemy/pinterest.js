e806e521ac09624202a2abafe575c10d0d22477f
Czarna Owca <szydlowska_klaudia@protonmail.com>
2022-05-13 15:39:17 +0200

    pinterest

---
+++ cypress/integration/udemy/pinterest.js
@@ -0,0 +1,14 @@
+describe("homepage", () => {
+    
+   beforeEach(() => {
+        cy.visit("https://www.pinterest.com/");
+
+    })
+
+    it("should have pinterest logo in bottom bar", ()=> {
+        
+        //cy.visit("https://www.pinterest.com/")
+       //cy.get(".xvE zI7 iyn Hsu")//.should("contain.text", Pinterest)
+
+    })
+})
---
+++ cypress/integration/yt/kurs_spec.js
@@ -0,0 +1,31 @@
+
+//describe("siepomaga", () => {
+    //it("add donation to cart", () => {
+     //   cy.visit("https://www.siepomaga.pl/fundacja-siepomaga");
+       // cy.contains("a", "Wesprzyj jednorazowo").click({ force: true });
+       // cy.contains("div", "payment-choose-amount-component__fixed-value-inputs__element__chip").contains('50').click({ force: true });
+     //   cy.get('input[name="payment[signature]"]').type("stefan");
+       // cy.contains("button", "Do koszyka darowizn").click();
+       // cy.contains("div.ui", "1");
+       // cy.contains("a", "Opłać koszyk darowizn").click({ force: true });
+   // })
+
+
+  // beforeEach(() => { cy.visit("https://www.siepomaga.pl/fundacja-siepomaga") })
+
+  // it("page has logo in bottom", () =>{
+      
+       //cy.get(".logo-img").should("include","https://www.siepomaga.pl/assets/logo-9bf43a17715eeb8afadbe2faefc6e8d681ed0c6ba9df94a1a6267def1770f660.svg")
+
+   
+  // })
+
+  // it("has ")
+//})
+describe(" pin", ()=>{
+    beforeEach(() => {
+    cy.visit("https://todomvc.com/examples/vanillajs/");
+
+    })
+
+})
